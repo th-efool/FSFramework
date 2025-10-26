0631b6944edc18c82b70b052667cb03ed73516a6
KUMAR MANISH SINGH <59444327+th-efool@users.noreply.github.com>
2025-10-26 07:10:51 +0530

    AddingInterfacesDefinition

---
+++ Plugins/FSPlugin/Source/FSGameplay/Private/Systems/Interface/FSInteractInterface.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Systems/Interface/FSInteractInterface.h"
+
+
+// Add default functionality here for any IFSInteract functions that are not pure virtual.
---
+++ Plugins/FSPlugin/Source/FSGameplay/Private/Systems/Interface/FSPlayerInterface.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Systems/Interface/FSPlayerInterface.h"
+
+
+// Add default functionality here for any IFSPlayer functions that are not pure virtual.
